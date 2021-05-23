7e423d80e915d5395af16344493a6918de6feb9e
pm-ribeiro <patricia.ribeiro@melhorenvio.com>
2021-05-23 17:10:31 -0300

    feat: display data usando JS e map

--- src/components/MyShows.vue
+++ src/components/MyShows.vue
@@ -1,16 +1,73 @@
-<template>
-  <h3>Hello</h3>
-</template>
 <script>
 
 import {h, ref} from "vue";
 
 export default {
   setup() {
+    let inputValue = ref(null),
+        shows = ref(null),
+        error = ref(null);
+
+    const searchText = async search => {
+      if (!search) return;
+      console.log('got here', search);
+
+      const searchT = await fetch(
+        `http://api.tvmaze.com/search/shows?q=${search}`
+      ).catch(e => {
+        error.value = "error" + e;
+        console.log("Got error: ", error.value);
+        return;
+      });
+
+      shows.value = await searchT.json().catch(e => (error.value = e));
+      console.log('Show = ', shows.value);
+    };
+
+
     return () => h(
-      "div",
-      {},
-      "A Test"
+      "form",
+      {
+        style: "max-width: 768px; margin: auto",
+        onSubmit: event => event.preventDefault()
+      },
+      // Children
+      [
+        // The input field
+        h(
+          "input",
+          {
+            onInput: event => {
+              {
+                inputValue = event.target.value;
+                // console.log(inputValue); // Just a test
+              }
+            }
+          }
+        ),
+        // The search button
+        h(
+          "button",
+          {
+            onClick: async () => { searchText(inputValue) }
+          },
+          "search"
+        ),
+
+        // Display the shows or a error
+        h("div", { class: "shows" },[
+          error.value
+          ? h("div", error.value) :
+          shows.value?.map((show) =>
+            h("div", {class: "show"},
+              [
+                h("h3", show.show.name),
+                h("img", { src: show.show.image?.medium })
+              ]
+            )
+          )
+        ])
+      ]
     );
   }
 };
