838df4a457e451d80e0c6e2c8d5c557a2c35a646
pm-ribeiro <patricia.mr3.14@gmail.com>
2021-06-06 17:46:04 -0300

    feat: adiciona element+ input para teste

---
+++ src/components/ElementInput.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-input placeholder="Please input" v-model="input"></el-input>
+  <span>
+    {{input}}
+  </span>
+</template>
+
+<script>
+  import { defineComponent, ref } from 'vue'
+export default defineComponent ({
+  setup() {
+    return {
+      input: ref('')
+    }
+  }
+})
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+createApp(App).use(ElementPlus).mount('#app')
