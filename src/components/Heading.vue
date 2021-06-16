5488f06727e66dcde021d246c0c38f4a56d5287c
pm-ribeiro <patricia.mr3.14@gmail.com>
2021-06-16 17:58:27 -0300

    fix: add style

--- src/components/Heading.vue
+++ src/components/Heading.vue
@@ -15,7 +15,7 @@ export default {
     console.log("slot =", slot);
 
     const modifiedSlot = slot.map(({ children }) => {
-      return h("div", { style: "color : purple" }, children);
+      return h("div", { style: "color : blue; font-weight: bold" }, children);
     });
 
     // params: html element, attrs, "children"
